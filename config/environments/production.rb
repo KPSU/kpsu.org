42be2cd97246d2cc1d6af6bf9925946e2fbb4975
S. Salazar <servando@gmail.com>
2012-01-16 21:07:36 -0800

    program fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Kpsu::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
-
+  config.threadsafe!
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
