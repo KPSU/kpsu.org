8e494de2d3d4a34b60432e10a3400e7c95b606e8
jake <you@example.com>
2012-04-10 22:40:03 -0700

    preparing for deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 set :application, "kpsu.org"
-set :user, "seve"
+set :user, "jake"
 set :scm, :git
 set :scm_verbose, true
 set :deploy_to, "/var/www/kpsu.org"
-set :repository, "ssh://seve@kpsu.org:6666/var/git/kpsu.org"
+set :repository, "ssh://jake@kpsu.org:6666/var/git/kpsu.org"
 set :port, 6666
 set :ssh_options, {:forward_agent => true}
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
