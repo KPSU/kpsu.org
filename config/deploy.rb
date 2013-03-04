18bb83648c2b70437e40938a0b1f7c3c711d0b13
Michael Zemel <admin@kpsu.org>
2013-03-03 22:48:06 -0800

    Changed the deploy file in a first attempt to get capistrano deployment (cap deploy) to succeed.  More may come.

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,8 +19,14 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cd /var/www/kpsu.org/current; #{sudo} /opt/ruby-1.9.3/bin/bundle install"
-    run "#{sudo} /etc/init.d/thin restart"
+	run "cd /var/www/kpsu.org/current; #{sudo} bundle install"  
+    #run "cd /var/www/kpsu.org/current; #{sudo} /opt/ruby-1.9.3/bin/bundle install"
+    run "#{sudo} thin restart --all /etc/thin"
+    #run "#{sudo} /etc/init.d/thin restart"
+    #the above was the stuff commented out, which worked well for a very long time.  Now I notice,
+    #on the server, that the old code would not work and is what the attempted cap deploy fails at.
+    #assuming the new, non-commented-out code will work but is something we should investigate if
+    #there is a surplus of time and interest.
     run "curl -H \"x-api-key:8225b636618d301764fa0a88832c9d64af71f31f8e961b9\" -d \"deployment[application_id]=301521\" -d \"deployment[host]=kpsu.org\" -d \"deployment[user]=#{user}\"  https://rpm.newrelic.com/deployments.xml"
   end
 
