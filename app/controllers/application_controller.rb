02644a19fe620e5a2e0b1b1a739cb9e1cc17e270
Michael <admin@kpsu.org>
2013-03-29 13:18:36 -0700

    Put twitter keys in config/initializers/twitter.rb and access them through application controller as Rails.application.config.twitter_whatever

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -71,19 +71,19 @@ class ApplicationController < ActionController::Base
   end
   
   def kpsu_twitter_token
-    "161043625-TxvxqYwBytqXUUl39OsSG69nKWE627ytO5eD9yGA"
+    Rails.application.config.kpsu_twitter_token
   end
   
   def kpsu_twitter_secret
-    "cUJcwjodXgwwdIO6GU7Urjd9UnfZb1nJBr6ojR8NWEw"
+    Rails.application.config.kpsu_twitter_secret
   end
   
   def twitter_consumer_secret
-    "q2yX19uuy2izSHKKcunNEMjk1BCGjnEeRg0JCCMF18"
+    Rails.application.config.twitter_consumer_secret
   end
   
   def twitter_consumer_key
-    "GRpxk6zjTKk3o7wTuXiSw"
+    Rails.application.config.twitter_consumer_key
   end
   
   def twitter_client
