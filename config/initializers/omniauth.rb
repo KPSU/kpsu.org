005485e751ee8c80f9a43ca24671a370c0081ca1
Michael <admin@kpsu.org>
2013-03-29 15:51:42 -0700

    Removing sensitive files from my index.

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-require 'openid/store/filesystem'
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter,  Rails.application.config.twitter_consumer_key, Rails.application.config.twitter_consumer_secret
-  provider :facebook, "", "", {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
-  provider :open_id,  OpenID::Store::Filesystem.new('/tmp')
-end
--- config/initializers/twitter.rb
+++
@@ -1,8 +0,0 @@
-Kpsu::Application.configure do
-
-	config.kpsu_twitter_token = ""
-	config.kpsu_twitter_secret = ""
-	config.twitter_consumer_secret = ""
-	config.twitter_consumer_key = ""
-
-end
