34aa9a7117876f1cc0860c0f29aa5f174991f59f
Server <admin@kpsu.org>
2013-03-30 01:10:01 +0000

    Removed config files.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Kpsu::Application.config.secret_token = '43acff9fd2c0d4eb404d8029032baa8e8dce37bc93088db07f1596cbb12ce5e8d8663b3db22a1621931bc2f705e060855b959c19724aaa29d36fac43e0a87ee6'
