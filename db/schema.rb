b5452a6e5b918305fa19b76b6e0f4d34ce7afab7
Server <admin@kpsu.org>
2013-04-05 07:21:26 +0000

    Serverside changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130405030610) do
+ActiveRecord::Schema.define(:version => 20130405063043) do
 
   create_table "abilities", :force => true do |t|
     t.integer  "role_id"
