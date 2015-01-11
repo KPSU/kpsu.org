8825967ba49575a387305b716c80c367f7bc6f6b
Sean DeBellis <debellis@pdx.edu>
2015-01-10 16:41:27 -0800

    change message

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -9,7 +9,7 @@ class AppsController < ApplicationController
         if @applicant.save
             redirect_to('/', :notice => "Application was successfully sent.")
         else
-            flash[:alert] = "Application Not Submitted! Please Fill In Highlighted Fields!"
+            flash[:alert] = "Application Not Submitted! Please Fill In Highlighted Fields"
             render 'new'
         end
     end
